acc585accf1d7ef1ad34ee9f60b586436965f3aa
Alisa Vishniakova <vishnyakova.alisa@gmail.com>
2025-03-12 00:01:29 +0700

    добавила 3 теста для full_size

--- main.cpp
+++ main.cpp
@@ -39,3 +39,21 @@ TEST(CandleTest, Contains_BoundaryLow)
     Candle candle = Candle(100, 200, 50, 150);
     EXPECT_TRUE(candle.contains(50));
 }
+
+TEST(CandleTest, FullSize_1)
+{
+    Candle candle = Candle(100, 200, 50, 150);
+    EXPECT_EQ(candle.full_size(), 150);
+}
+
+TEST(CandleTest, FullSize_2)
+{
+    Candle candle = Candle(100, 210, 40, 150);
+    EXPECT_EQ(candle.full_size(), 170);
+}
+
+TEST(CandleTest, FullSize_3)
+{
+    Candle candle = Candle(100, 300, 140, 100);
+    EXPECT_EQ(candle.full_size(), 160);
+}
